11b9ddd544d5c2e6f33e50c24c30b686bc136f33
Amit Ayare <amit.ayare@techpartner.in>
2024-11-07 15:12:09 +0530

    dummy-app-code

---
+++ src/main/java/com/example/EurekaClientApplication.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Eureka Client!";
+    }
+}
+
